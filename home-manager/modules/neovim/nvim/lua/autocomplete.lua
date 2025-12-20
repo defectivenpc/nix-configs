6990518fc582fcbca601c1e9969c0ef586eb0cf6
Michael Whitehead <onepunchlinux@gmail.com>
2025-12-20 13:25:58 -0700

    add auto complete

--- home-manager/modules/neovim/nvim/lua/autocomplete.lua
+++ home-manager/modules/neovim/nvim/lua/autocomplete.lua
@@ -1,56 +1,79 @@
 local lspkind = require("lspkind")
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 cmp.setup({
-  snippet = {
-    expand = function(args)
-        require('luasnip').lsp_expand(args.body)
-      end,
-  },
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
 
-  mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  },
+	completion = {
+		autocomplete = { cmp.TriggerEvent.TextChanged },
+		completeopt = "menu,menuone,noinsert",
+	},
 
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'buffer' },
-    { name = 'orgmode' },
-  }),
+	mapping = cmp.mapping.preset.insert({
+		-- Navigate
+		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 
-  formatting = {
-    format = lspkind.cmp_format({
-      maxwidth = 50,
-      elispis_char = "...",
-    }),
-  }
+		-- Accept (fast, no Enter hijack)
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+		["<C-x><C-o>"] = cmp.mapping.complete(),
+
+		-- Cancel
+		["<C-e>"] = cmp.mapping.abort(),
+
+		-- Scroll docs
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+
+		-- Optional: keep Enter clean
+		["<CR>"] = cmp.mapping({
+			i = function(fallback)
+				if cmp.visible() then
+					cmp.confirm({ select = true })
+				else
+					fallback()
+				end
+			end,
+		}),
+	}),
+
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "orgmode" },
+	}),
+
+	formatting = {
+		format = lspkind.cmp_format({
+			maxwidth = 50,
+			elispis_char = "...",
+		}),
+	},
 })
 
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
-  }
+cmp.setup.cmdline("/", {
+	sources = {
+		{ name = "buffer" },
+	},
 })
 
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+cmp.setup.cmdline(":", {
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
 })
 
 -- Setup lspconfig.
 --local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 --require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-  --capabilities = capabilities
-  --}
+--capabilities = capabilities
+--}
--- home-manager/modules/neovim/nvim/lua/autopairs.lua
+++ home-manager/modules/neovim/nvim/lua/autopairs.lua
@@ -1,16 +1,16 @@
-local autopairs = require('nvim-autopairs')
+local autopairs = require("nvim-autopairs")
 
 autopairs.setup({
-  check_ts = true,
-  ts_config = {
-    lua = { "string" },
-    javascript = { "template_string" },
-    java = false,
-  }
+	check_ts = true,
+	ts_config = {
+		lua = { "string" },
+		javascript = { "template_string" },
+		java = false,
+	},
 })
 
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
